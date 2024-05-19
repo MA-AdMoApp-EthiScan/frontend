7cee329d41d7136ac689da227c4f2041623e1884
Clarisse Fleurimont <clarisse.fleurimont@heig-vd.ch>
2024-05-19 13:45:56 +0200

    Ajout message si aucun favori

--- lib/presentation/favorites/favorites_page.dart
+++ lib/presentation/favorites/favorites_page.dart
@@ -19,15 +19,6 @@ class FavoritesPage extends StatefulWidget {
   final EthiscanUser user;
   const FavoritesPage(this.user, {super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   @override
   State<FavoritesPage> createState() => _FavoritesPage();
 }
@@ -268,6 +259,11 @@ class _FavoritesPage extends State<FavoritesPage> {
           padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
           child: CustomCircularLoading(),
         ),
+      ];
+    } else if (favorites.isEmpty) {
+      return [
+        CustomH3(I18nUtils.translate(context, "favorites.empty.title")),
+        CustomText(I18nUtils.translate(context, "favorites.empty.message"))
       ];
     } else {
       //favorites = favorites.isEmpty ? ["test", "test 2"] : favorites;
