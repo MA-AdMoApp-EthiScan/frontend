08ad7f8d1163406b295bc626c6199e3b519b1aaf
ODAncona <olivier.dancona@heig-vd.ch>
2024-05-20 23:01:42 +0200

    auth provider

--- lib/data/datasources/auth_provider.dart
+++ lib/data/datasources/auth_provider.dart
@@ -5,15 +5,17 @@ import 'package:ethiscan/utils/exceptions.dart';
 
 @Singleton(as: AuthRepository)
 class AuthenticationProvider implements AuthRepository {
-  AuthenticationProvider();
+  final FirebaseAuth firebaseAuth;
+
+  AuthenticationProvider(this.firebaseAuth);
 
   // ~~~ Authentication ~~~
   @override
   Future<UserCredential> logIn(
       {required String email, required String password}) async {
     try {
-      return await FirebaseAuth.instance
-          .signInWithEmailAndPassword(email: email, password: password);
+      return await firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: password);
     } on FirebaseAuthException {
       rethrow;
     }
@@ -21,81 +23,22 @@ class AuthenticationProvider implements AuthRepository {
 
   @override
   Future<void> logOut() {
-    return FirebaseAuth.instance.signOut();
+    return firebaseAuth.signOut();
   }
 
   @override
   bool isUserConnected() {
-    return FirebaseAuth.instance.currentUser != null;
+    return firebaseAuth.currentUser != null;
   }
 
-  // @override
-  // String get uid {
-  //   User? user = FirebaseAuth.instance.currentUser;
-  //   if (user == null) {
-  //     throw StateError("Not connected");
-  //   }
-  //   return user.uid;
-  // }
-
-  // ~~~ Registration ~~~
   @override
   Future<UserCredential> signUp(
       {required String email, required String password}) async {
     try {
-      return await FirebaseAuth.instance
-          .createUserWithEmailAndPassword(email: email, password: password);
+      return await firebaseAuth.createUserWithEmailAndPassword(
+          email: email, password: password);
     } on FirebaseAuthException catch (e) {
       throw getAuthenticationExceptionFromCode(e.code);
     }
   }
-
-  // @override
-  // Future<void> sendEmailVerification() async {
-  //   User? user = FirebaseAuth.instance.currentUser;
-  //   if (user == null) {
-  //     throw StateError("Not connected");
-  //   }
-  //   user.sendEmailVerification();
-  // }
-
-  // @override
-  // Future<bool> isEmailInUse(String emailAddress) async {
-  //   try {
-  //     // Fetch sign-in methods for the email address
-  //     final list =
-  //         await FirebaseAuth.instance.fetchSignInMethodsForEmail(emailAddress);
-
-  //     // In case list is not empty
-  //     if (list.isNotEmpty) {
-  //       // Return true because there is an existing
-  //       // user using the email address
-  //       return true;
-  //     } else {
-  //       // Return false because email address is not in use
-  //       return false;
-  //     }
-  //   } on FirebaseAuthException catch (e) {
-  //     throw getAuthenticationExceptionFromCode(e.code);
-  //   }
-  // }
-
-  // @override
-  // String getUserEmail() {
-  //   User? user = FirebaseAuth.instance.currentUser;
-  //   if (user == null) {
-  //     throw StateError("Not connected");
-  //   }
-  //   return user.email!;
-  // }
-
-  // @override
-  // bool get isVerified {
-  //   User? user = FirebaseAuth.instance.currentUser;
-  //   if (user == null) {
-  //     throw StateError("Not connected");
-  //   }
-  //   user.reload();
-  //   return user.emailVerified;
-  // }
 }
--- test/data/datasources/auth_provider_test.dart
+++ test/data/datasources/auth_provider_test.dart
@@ -2,20 +2,30 @@ import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ethiscan/data/datasources/auth_provider.dart';
 
+final tUser = MockUser(
+  isAnonymous: false,
+  uid: 'T3STU1D',
+  email: 'bob@thebuilder.com',
+  displayName: 'Bob Builder',
+  phoneNumber: '0800 I CAN FIX IT',
+  photoURL: 'http://photos.url/bobbie.jpg',
+  refreshToken: 'some_long_token',
+);
+
 void main() {
   late AuthenticationProvider authProvider;
   late MockFirebaseAuth mockAuth;
 
   setUp(() {
-    mockAuth = MockFirebaseAuth();
-    authProvider = AuthenticationProvider();
+    mockAuth = MockFirebaseAuth(mockUser: tUser);
+    authProvider = AuthenticationProvider(mockAuth);
   });
 
   group('AuthProvider', () {
     test('logIn logs in user with correct credentials', () async {
       final user = MockUser(email: 'test@example.com');
       mockAuth = MockFirebaseAuth(mockUser: user);
-      authProvider = AuthenticationProvider();
+      authProvider = AuthenticationProvider(mockAuth);
 
       final result = await authProvider.logIn(
           email: 'test@example.com', password: 'password');
